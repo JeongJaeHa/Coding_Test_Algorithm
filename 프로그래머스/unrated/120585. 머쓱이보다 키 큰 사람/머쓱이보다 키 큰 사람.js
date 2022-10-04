c5031896237e3d5ef920c52ee029abf2ef0c13e3
JaeHaaaa <99805929+JeongJaeHa@users.noreply.github.com>
2022-10-04 23:10:41 +0900

    [unrated] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,9 +1,4 @@
 function solution(array, height) {
     var answer = 0;
-    for(let i=0; i<array.length; i++) {
-        if(array[i] > height) {
-            answer++
-        }
-    }
-    return answer;
+    return answer = array.filter((el)=>{return el > height;}).length;
 }
