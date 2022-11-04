00771265da79580df81b10f3a91d113469fcc0e5
JaeHaaaa <99805929+JeongJaeHa@users.noreply.github.com>
2022-11-04 18:00:24 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -0,0 +1,7 @@
+function solution(box, n) {
+  let width = parseInt(box[0]/ n);
+  let vertical = parseInt(box[1]/n);
+  let height = parseInt(box[2]/n);
+  
+  return answer = width*vertical*height
+}
