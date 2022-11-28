905fbfe261ad280cb07554c954e68058c8faabcd
JaeHaaaa <99805929+JeongJaeHa@users.noreply.github.com>
2022-11-28 09:16:17 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,9 +1,3 @@
 function solution(s) {
-  let answer = 0
-  let arr = [...String(s)]
-  for(let i=0; i<arr.length; i++) {
-    answer += Number(arr[i])
-    
-  }
-  return answer
+  return [...String(s)].reduce((a, b) => Number(a) + Number(b), 0);
 }
